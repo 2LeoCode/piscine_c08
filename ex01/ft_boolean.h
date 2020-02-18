2475b0747893ced36a62cb7a99b403f851e9257c
Your Name <you@example.co>
2020-02-18 23:54:40 +0100

    lol

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/18 13:24:42 by lsuardi           #+#    #+#             */
+/*   Updated: 2020/02/18 14:14:05 by lsuardi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+# define t_bool int
+# define EVEN(x) x % 2 == 0
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+#endif
