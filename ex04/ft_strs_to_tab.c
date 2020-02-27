793f26c4d2404cb23051eec830300848e1c05116
Your Name <you@example.co>
2020-02-27 16:06:51 +0100

    last push (vraiment lol)

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: lsuardi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 00:02:19 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/02/26 18:45:01 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/02/27 16:05:58 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,20 +46,15 @@ struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 	struct s_stock_str	*lol;
 
 	lol = 0;
-	if (malloc(sizeof(struct s_stock_str) * ac + 1) == NULL)
+	if (!(lol = malloc(sizeof(struct s_stock_str) * (ac + 1))))
 		return (NULL);
-	lol = malloc(sizeof(struct s_stock_str) * ac + 1);
 	i = 0;
 	while (i < ac - 1)
 	{
 		lol[i].size = ft_strlen(av[i]);
 		lol[i].str = av[i];
-		if ((char*)malloc(sizeof(char) * lol[i].size + 1) == NULL)
-		{
-			lol = 0;
-			return (NULL);
-		}
-		lol[i].copy = ft_strdup(av[i]);
+		if (!(lol[i].copy = ft_strdup(av[i])))
+			return (NULL)
 		i++;
 	}
 	lol[i].str = 0;
